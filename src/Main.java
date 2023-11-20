205e9ecf7b994229f8ec53fd9b90a048a8fbbf99
RustamAleev2023 <ridermix@yandex.ru>
2023-11-20 10:46:11 +0200

    Tasks2,3 done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      task1();
+//      task1();
+//      task2();
+//      task3();
+      task4();
     }
 
     //Task1
@@ -20,5 +23,42 @@ public class Main {
         units = scanner.nextInt();
 
         System.out.println(hundreds * 100 + tens * 10 + units);
+    }
+
+    //Task2
+    public static void task2(){
+        System.out.println("Введите число, состоящее из четырех цифр");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int num4 = number % 10;
+        number /= 10;
+        int num3 = number % 10;
+        number /= 10;
+        int num2 = number % 10;
+        number /= 10;
+        int num1 = number % 10;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num1 + " * " + num2 + " * " + num3 + " * " + num4 + " = " + num1 * num2 * num3 * num4 );
+    }
+
+    //Task3
+    public static void task3(){
+        System.out.println("Введите кол-во метров");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        System.out.println("В " + num + "м " + num * 100 + "см");
+        System.out.println("В " + num + "м " + num * 10 + "дм");
+        System.out.println("В " + num + "м " + num * 1000 + "мм");
+        System.out.println("В " + num + "м " + num / 1609.34 + " миль");
+    }
+
+    //Task4
+    public static void task4(){
+
     }
 }
